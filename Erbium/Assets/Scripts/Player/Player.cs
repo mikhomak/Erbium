a1c68356ec181ad14e63ed65d9d5c091207a8d61
Misha <homakhaker@gmail.com>
2020-01-15 01:06:54 +0100

    Added Player script

---
+++ Erbium/Assets/Scripts/Player/Player.cs
@@ -0,0 +1,18 @@
+﻿using Characters;
+using UnityEngine;
+
+namespace Player {
+    public class Player : MonoBehaviour, ICharacter {
+        public void die() {
+            throw new System.NotImplementedException();
+        }
+
+        public float getPhysicArmor() {
+            throw new System.NotImplementedException();
+        }
+
+        public float getMagicArmor() {
+            throw new System.NotImplementedException();
+        }
+    }
+}
