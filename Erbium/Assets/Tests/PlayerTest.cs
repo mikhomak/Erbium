4bd16528f2a80aab271f3825812c1000ec720481
Misha <homakhaker@gmail.com>
2020-01-17 02:11:39 +0100

    Added unitTests

---
+++ Erbium/Assets/Tests/PlayerTest.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Characters;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+namespace Tests
+{
+    public class PlayerTest
+    {
+
+        [UnityTest]
+        public IEnumerator NewTestScriptWithEnumeratorPasses()
+        {
+            GameObject player = MonoBehaviour.Instantiate(Resources.Load<GameObject>("Prefabs/Player/Player"));
+            yield return new WaitForEndOfFrame();
+            Assert.NotNull(player.GetComponent<ICharacter>());
+            Assert.NotNull(player.GetComponent<IPhysicsCharacter>());
+            Assert.NotNull(player.GetComponent<IPhysicsCharacter>().getRigidbody());
+            Assert.NotNull(player.GetComponent<IPhysicsCharacter>().getStats());
+            Assert.NotNull(player.GetComponent<IPhysicsCharacter>().getMovement());
+            Object.Destroy(player);
+        }
+    }
+}
