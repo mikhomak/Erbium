64762092e21098a00b700074b8ead4659f84fd97
Misha <homakhaker@gmail.com>
2020-01-21 00:08:56 +0100

    Created animation state base classes

---
+++ Erbium/Assets/Scripts/Animations/AnimatinoState.cs
@@ -0,0 +1,9 @@
+﻿using Animators;
+using UnityEngine;
+
+namespace Animations {
+    public class AnimationState : StateMachineBehaviour {
+        private ICharacterAnimator characterAnimator;
+        
+    }
+}
