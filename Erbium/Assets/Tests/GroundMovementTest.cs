536455dc0f2e9a892ad95cc3574080682e61b1bf
Misha <homakhaker@gmail.com>
2020-01-22 02:53:35 +0100

    Added nsubstitute for the mocking in testing

--- Erbium/Assets/Tests/GroundMovementTest.cs
+++ Erbium/Assets/Tests/GroundMovementTest.cs
@@ -9,7 +9,7 @@ using UnityEngine.TestTools;
 namespace Tests {
     public class GroundMovementTest {
         private GameObject playerGo;
-
+        private ICharacter player;
         [SetUp]
         public void setUpTestScene() {
             MonoBehaviour.Instantiate(Resources.Load<GameObject>("Prefabs/Cameras/Main Camera"));
@@ -20,19 +20,32 @@ namespace Tests {
             playerGo = MonoBehaviour.Instantiate(Resources.Load<GameObject>("Prefabs/Player/Player"));
             var inputManager = inputManagerGO.GetComponent<InputManager>();
             inputManager.playerGameObject = playerGo;
+            player = playerGo.GetComponent<ICharacter>();
+
         }
 
         [UnityTest]
-        public IEnumerator initalizeGroundMovement() {
+        public IEnumerator initalizeGroundMovementTest() {
             yield return new WaitForEndOfFrame();
 
-            ICharacter player = playerGo.GetComponent<ICharacter>();
             Assert.NotNull(player);
 
             IMovement movement = player.getMovement();
             Assert.NotNull(movement);
             
             Assert.True(movement is GroundMovement);
+        }
+
+        [UnityTest]
+        public IEnumerator moveTest() {
+            
+            
+            
+            
+            player.getMovement().move(new Vector3(10,0,10));
+            yield return new WaitForSeconds(3f);
+            Debug.Log(playerGo.transform.position);
+            Assert.Equals(1, 1);
         }
     }
 }
