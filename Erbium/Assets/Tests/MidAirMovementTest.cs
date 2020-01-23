be49d03aed00a8f9a11843d794139bf6658d2b2c
Misha <homakhaker@gmail.com>
2020-01-23 23:22:42 +0100

    Added init test for midair

--- Erbium/Assets/Tests/MidAirMovementTest.cs
+++ Erbium/Assets/Tests/MidAirMovementTest.cs
@@ -1,5 +1,50 @@
-﻿namespace Tests {
+﻿using System.Collections;
+using System.Collections.Generic;
+using Characters.Movement;
+using General;
+using NUnit.Framework;
+using Player;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+namespace Tests {
     public class MidAirMovementTest {
-        
+        private List<GameObject> gameObjects = new List<GameObject>();
+        private GameObject playerGo;
+        private IPlayer player;
+
+        [SetUp]
+        public void setUpTestScene() {
+            gameObjects = TestCommonMethods.init();
+            GameObject inputManagerGo = TestCommonMethods.initInputManager();
+            InputManager inputManager = inputManagerGo.GetComponent<InputManager>();
+            playerGo = TestCommonMethods.initPlayer(inputManager);
+            gameObjects.Add(inputManagerGo);
+            gameObjects.Add(playerGo);
+            player = playerGo.GetComponent<IPlayer>();
+        }
+
+        [TearDown]
+        public void afterTest() {
+            gameObjects.ForEach(Object.Destroy);
+            player = null;
+        }
+
+        [UnityTest]
+        public IEnumerator initTest() {
+            Assert.NotNull(player);
+            var transformPosition = playerGo.transform.position;
+            transformPosition.y = 100f;
+            playerGo.transform.position = transformPosition;
+            IMovement movement = player.getMovement();
+            Assert.True(movement is GroundMovement);
+
+            yield return new WaitForSeconds(1f);
+
+            movement = player.getMovement();
+            Assert.NotNull(movement);
+
+            Assert.True(movement is MidairMovement);
+        }
     }
 }
