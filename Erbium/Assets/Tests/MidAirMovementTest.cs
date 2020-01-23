b764a5ca998273f93028294da42d2817f31ff333
Misha <homakhaker@gmail.com>
2020-01-23 22:56:53 +0100

    Created TestCommonMethods

---
+++ Erbium/Assets/Tests/MidAirMovementTest.cs
@@ -0,0 +1,5 @@
+﻿namespace Tests {
+    public class MidAirMovementTest {
+        
+    }
+}
---
+++ Erbium/Assets/Tests/TestCommonMethods.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace Tests {
+    public class TestCommonMethods {
+
+
+        public static List<GameObject> init() {
+            List<GameObject> gameObjects = new List<GameObject> {
+                Object.Instantiate(Resources.Load<GameObject>("Prefabs/Cameras/Main Camera")),
+                Object.Instantiate(Resources.Load<GameObject>("Prefabs/Cameras/Always Forward Camera")),
+                Object.Instantiate(Resources.Load<GameObject>("Prefabs/Cameras/Camera Manager")),
+                Object.Instantiate(Resources.Load<GameObject>("Prefabs/Enviroments/Test Floor"))
+            };
+            var inputManagerGo =
+                Object.Instantiate(Resources.Load<GameObject>("Prefabs/General/Input Manager"));
+            var playerGo = Object.Instantiate(Resources.Load<GameObject>("Prefabs/Player/Player"));
+            gameObjects.Add(inputManagerGo);
+            gameObjects.Add(playerGo);
+            return gameObjects;
+        } 
+        
+    }
+}
