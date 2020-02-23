7bf2a5b11cba87ea3c6ec115640bac3e7d31e226
Misha <homakhaker@gmail.com>
2020-02-23 20:17:17 +0100

    Created attackTest

---
+++ Erbium/Assets/Tests/AttackTest.cs
@@ -0,0 +1,7 @@
+﻿namespace Tests {
+    public class AttackTest: AbstractTest {
+        
+        
+        
+    }
+}
--- Erbium/Assets/Tests/SlidingMovementTest.cs
+++ Erbium/Assets/Tests/SlidingMovementTest.cs
@@ -1,10 +1,7 @@
 ﻿using System.Collections;
-using System.Collections.Generic;
 using Characters.Movement;
-using General;
 using NSubstitute;
 using NUnit.Framework;
-using Player;
 using Player.MovementDirection;
 using UnityEngine;
 using UnityEngine.TestTools;
