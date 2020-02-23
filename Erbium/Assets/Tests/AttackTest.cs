a28a789fa84c3ac927325c898d7b4a0b9ee687fd
Misha <homakhaker@gmail.com>
2020-02-23 20:42:04 +0100

    Added characterHasMoved tests

--- Erbium/Assets/Tests/AttackTest.cs
+++ Erbium/Assets/Tests/AttackTest.cs
@@ -6,19 +6,41 @@ using UnityEngine.TestTools;
 
 namespace Tests {
     public class AttackTest : AbstractTest {
-
-
         [UnityTest]
         public IEnumerator attackManagerTest() {
             yield return new WaitForEndOfFrame();
             Assert.IsNotNull(player.getAttackManager());
         }
-        
+
         [UnityTest]
         public IEnumerator attackingMovementTest() {
             player.getAttackManager().fastAttack();
             yield return new WaitForFixedUpdate();
             Assert.IsTrue(player.getMovement() is AttackingMovement);
+        }
+
+        [UnityTest]
+        public IEnumerator rootMotionTest() {
+            Assert.IsFalse(player.getAnimatorFacade().getAnimator().applyRootMotion);
+            player.getAttackManager().fastAttack();
+            yield return new WaitForFixedUpdate();
+            Assert.IsTrue(player.getAnimatorFacade().getAnimator().applyRootMotion);
+        }
+
+        [UnityTest]
+        public IEnumerator characterHasMovedDuringFastAttack() {
+            var initPos = player.getRigidbody().position;
+            player.getAttackManager().fastAttack();
+            yield return new WaitForSeconds(1);
+            Assert.IsFalse(Equals(initPos, player.getRigidbody().position));
+        }
+
+        [UnityTest]
+        public IEnumerator characterHasMovedDuringStrongAttack() {
+            var initPos = player.getRigidbody().position;
+            player.getAttackManager().strongAttack();
+            yield return new WaitForSeconds(1);
+            Assert.IsFalse(Equals(initPos, player.getRigidbody().position));
         }
     }
 }
